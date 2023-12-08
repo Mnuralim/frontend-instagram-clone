035ff8c3cb09d919a173d8e99e0535531d515d46
mnuralim <shadownur345.com>
2023-12-08 14:15:09 +0800

    update

--- app/[userId]/components/ReelProfile.tsx
+++ app/[userId]/components/ReelProfile.tsx
@@ -1,18 +1,41 @@
-import React from "react";
-import Link from "next/link";
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 interface Props {
   posts: IPost[];
 }
 
 const ReelProfile = ({ posts }: Props) => {
+  const pathName = usePathname();
+  const [screenSize, setScreenSize] = useState<number>(0);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setScreenSize(window.innerWidth);
+
+      const handleResize = () => {
+        setScreenSize(window.innerWidth);
+      };
+
+      window.addEventListener('resize', handleResize);
+
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }
+  }, []);
   return (
     <section className="flex flex-col pt-1 text-white w-full">
       <div className="grid grid-cols-3 gap-[2px] md:grid-cols-4">
         {posts.map(
           (post) =>
-            post.type === "reel" && (
-              <Link href={"/"} key={post._id} className="aspect-[13/20] hover:opacity-50">
+            post.type === 'reel' && (
+              <Link
+                href={screenSize <= 768 ? `/p/${post._id}` : `${pathName}/?post=tab&p=${post._id}`}
+                key={post._id}
+                className="aspect-[13/20] hover:opacity-50"
+              >
                 <video autoPlay muted src={post.media} loop className="object-cover w-full h-full" />
               </Link>
             )
