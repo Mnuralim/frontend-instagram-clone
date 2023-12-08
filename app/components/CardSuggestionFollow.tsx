af846cb5bfb964820b676c8d9f4d1b98de719bea
mnuralim <shadownur345.com>
2023-12-08 19:02:32 +0800

    update

--- app/components/CardSuggestionFollow.tsx
+++ app/components/CardSuggestionFollow.tsx
@@ -1,9 +1,10 @@
-import React from "react";
-import Image from "next/image";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { FreeMode } from "swiper/modules";
-import "swiper/css";
-import "swiper/css";
+import React from 'react';
+import Image from 'next/image';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css';
+import Link from 'next/link';
 
 interface Props {
   users: IUser[];
@@ -13,15 +14,37 @@ interface Props {
 const CardSuggestionFollow = ({ users, handleFollow }: Props) => {
   return (
     <div className="md:hidden px-3 mt-1">
-      <Swiper freeMode={true} grabCursor={true} modules={[FreeMode]} className="mySwiper" slidesPerView={2} spaceBetween={4}>
+      <Swiper
+        freeMode={true}
+        grabCursor={true}
+        modules={[FreeMode]}
+        className="mySwiper"
+        slidesPerView={2}
+        spaceBetween={4}
+      >
         {users?.map((user) => (
           <SwiperSlide className="flex justify-center items-center flex-col w-full" key={user._id}>
             <div className="flex bg-black rounded flex-col mb-3 py-3 gap-[10px] justify-center items-center border border-slate-400 border-opacity-50">
-              <Image src={user.profile.imageProfile} alt="profile" width={110} height={110} className="object-cover w-[110px] h-[110px] rounded-full" />
-              <h2 className="text-xs font-semibold text-[#F5F5F5]">{user.username}</h2>
+              <Link href={`/${user._id}/?tab=post`}>
+                <Image
+                  src={user.profile.imageProfile}
+                  alt="profile"
+                  width={110}
+                  height={110}
+                  className="object-cover w-[110px] h-[110px] rounded-full"
+                />
+              </Link>
+              <Link href={`/${user._id}/?tab=post`} className="text-xs font-semibold text-[#F5F5F5]">
+                {user.username}
+              </Link>
               <p className="text-xs text-[#A8A8A8]">Suggested for you</p>
-              <button onClick={() => handleFollow(user._id)} className={`${user?.alreadyFollow ? "bg-[#262626]" : "bg-[#0195f7]"}  py-[5px] w-[92%] mx-3 text-xs text-center rounded-md font-semibold hover:bg-[#A8A8A8]`}>
-                {user?.alreadyFollow ? "Following" : "Follow"}
+              <button
+                onClick={() => handleFollow(user._id)}
+                className={`${
+                  user?.alreadyFollow ? 'bg-[#262626]' : 'bg-[#0195f7]'
+                }  py-[5px] w-[92%] mx-3 text-xs text-center rounded-md font-semibold hover:bg-[#A8A8A8]`}
+              >
+                {user?.alreadyFollow ? 'Following' : 'Follow'}
               </button>
             </div>
           </SwiperSlide>
