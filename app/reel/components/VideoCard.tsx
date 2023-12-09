3b72b76ab752c30971c74af148f7feabe1b64113
mnuralim <shadownur345.com>
2023-12-09 13:30:10 +0800

    update

--- app/reel/components/VideoCard.tsx
+++ app/reel/components/VideoCard.tsx
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 interface Props {
   videoUrl: string;
 }
 
 const VideoCard = ({ videoUrl }: Props) => {
+  const vidRef = useRef<HTMLVideoElement>(null);
+  const videoContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5,
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      const entry = entries[0];
+      if (entry.isIntersecting) {
+        vidRef.current?.play();
+      } else {
+        vidRef.current?.pause();
+      }
+    }, options);
+
+    const currentVideoRef = vidRef.current;
+
+    if (currentVideoRef) {
+      observer.observe(currentVideoRef);
+    }
+
+    return () => {
+      if (currentVideoRef) {
+        observer.unobserve(currentVideoRef);
+      }
+    };
+  }, []);
   return (
-    <div className="">
-      <video src={videoUrl} autoPlay loop className="w-full h-full aspect-auto" />
+    <div ref={videoContainerRef} className="w-full h-full">
+      <video
+        src={videoUrl}
+        ref={vidRef}
+        autoPlay
+        loop
+        className="w-full h-full object-cover"
+      />
     </div>
   );
 };
--- app/reel/page.tsx
+++ app/reel/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import { Pagination, Mousewheel } from 'swiper/modules';
@@ -7,22 +7,44 @@ import { usePost } from '@/utils/swr';
 import { useSession } from 'next-auth/react';
 import { KeyedMutator } from 'swr';
 import VideoCard from './components/VideoCard';
+import { usePathname } from 'next/navigation';
 
 const Page = () => {
   const { data: session } = useSession();
+  const pathName = usePathname();
   const {
     post: posts,
     mutate,
     isLoading,
-  }: { post: IPost[]; mutate: KeyedMutator<any>; isLoading: boolean } = usePost(session?.user.token as string);
-  console.log(posts);
+  }: { post: IPost[]; mutate: KeyedMutator<any>; isLoading: boolean } = usePost(
+    session?.user.token as string
+  );
+
+  useEffect(() => {
+    if (pathName === '/reel') {
+      document.body.classList.add('overflow-hidden');
+    }
+
+    return () => {
+      document.body.classList.remove('overflow-hidden');
+    };
+  }, [pathName]);
+
   return (
-    <section className="h-screen md:max-w-[34%] md:mx-auto overflow-y-hidden">
-      <Swiper direction={'vertical'} modules={[Pagination, Mousewheel]} mousewheel={true} className="w-full h-full">
+    <section className="h-screen min-h-screen md:max-w-[34%] md:mx-auto overflow-y-hidden">
+      <Swiper
+        direction={'vertical'}
+        modules={[Pagination, Mousewheel]}
+        mousewheel={true}
+        className="w-full h-full"
+      >
         {posts?.map(
           (post) =>
             post.type === 'reel' && (
-              <SwiperSlide key={post._id}>
+              <SwiperSlide
+                key={post._id}
+                className="flex items-center justify-center w-full h-full"
+              >
                 <VideoCard videoUrl={post.media} />
               </SwiperSlide>
             )
