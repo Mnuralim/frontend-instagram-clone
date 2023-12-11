b656489a1022c5a21756ce389e2b72611a7d2383
mnuralim <shadownur345.com>
2023-12-11 23:32:31 +0800

    update

--- app/components/AllPost.tsx
+++ app/components/AllPost.tsx
@@ -4,9 +4,9 @@ import { useSession } from 'next-auth/react'
 import { usePost } from '@/utils/swr'
 import PostCard from '@/components/PostCard'
 import { KeyedMutator } from 'swr'
-import SuggestionFollow from './SuggestionFollow'
 import UploadingLoad from '@/components/UploadingLoad'
 import PostSkeleton from '@/components/skeleton/PostSkeleton'
+import SuggestionFollow from './SuggestionFollow'
 
 const AllPost = () => {
   const { data: session } = useSession()
