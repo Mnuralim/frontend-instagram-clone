263df3be8eb43556e0d9c2d3597ef67e2f26f8e7
mnuralim <shadownur345.com>
2024-05-25 15:44:31 +0800

    update

--- components/SearchList.tsx
+++ components/SearchList.tsx
@@ -1,29 +1,23 @@
-import Image from 'next/image';
-import React from 'react';
-import { AiOutlineClose } from 'react-icons/ai';
+import Image from 'next/image'
+import React from 'react'
+import { AiOutlineClose } from 'react-icons/ai'
 
 interface Props {
-  user?: IUser;
-  history?: IHistory;
-  isLoading: boolean;
-  addToHistory: (id: string) => {};
-  deleteHistory: (id: string) => {};
+  user?: IUser
+  history?: IHistory
+  isLoading: boolean
+  addToHistory: (id: string) => {}
+  deleteHistory: (id: string) => {}
 }
 
-const SearchList = ({
-  user,
-  isLoading,
-  history,
-  addToHistory,
-  deleteHistory,
-}: Props) => {
-  if (isLoading) return <p>loading</p>;
+const SearchList = ({ user, isLoading, history, addToHistory, deleteHistory }: Props) => {
+  if (isLoading) return <p>loading</p>
   return (
     <>
       {user && !history && (
         <div
           onClick={() => addToHistory(user._id)}
-          className="items-center relative flex justify-between px-2 py-2 hover:bg-[#1A1A1A] cursor-pointer md:px-5"
+          className="items-center relative flex justify-between px-2 py-2 hover:bg-[#1A1A1A] cursor-pointer md:px-5 overflow-y-auto"
         >
           <div className="flex items-center gap-2">
             <Image
@@ -34,9 +28,7 @@ const SearchList = ({
               className="object-cover w-12 h-12 rounded-full"
             />
             <div>
-              <h2 className="text-xs font-semibold text-[#F5F5F5]">
-                {user?.username}
-              </h2>
+              <h2 className="text-xs font-semibold text-[#F5F5F5]">{user?.username}</h2>
               <p className="text-xs text-[#A8A8A8]">{user?.profile.fullName}</p>
             </div>
           </div>
@@ -44,10 +36,7 @@ const SearchList = ({
       )}
       {!user && history && (
         <div className="items-center relative flex justify-between px-2 py-2 hover:bg-[#1A1A1A] cursor-pointer md:px-5">
-          <div
-            onClick={() => addToHistory(history.savedUser._id)}
-            className="flex items-center gap-2 w-[90%]"
-          >
+          <div onClick={() => addToHistory(history.savedUser._id)} className="flex items-center gap-2 w-[90%]">
             <Image
               src={history.savedUser.profile.imageProfile}
               alt="profile"
@@ -56,24 +45,17 @@ const SearchList = ({
               className="object-cover w-12 h-12 rounded-full"
             />
             <div>
-              <h2 className="text-xs font-semibold text-[#F5F5F5]">
-                {history.savedUser.username}
-              </h2>
-              <p className="text-xs text-[#A8A8A8]">
-                {history.savedUser.profile.fullName}
-              </p>
+              <h2 className="text-xs font-semibold text-[#F5F5F5]">{history.savedUser.username}</h2>
+              <p className="text-xs text-[#A8A8A8]">{history.savedUser.profile.fullName}</p>
             </div>
           </div>
-          <button
-            onClick={() => deleteHistory(history._id)}
-            className="hover:scale-110"
-          >
+          <button onClick={() => deleteHistory(history._id)} className="hover:scale-110">
             <AiOutlineClose size="20" color="#A8A8A8" />
           </button>
         </div>
       )}
     </>
-  );
-};
+  )
+}
 
-export default SearchList;
+export default SearchList
--- components/SearchMenu.tsx
+++ components/SearchMenu.tsx
@@ -53,26 +53,28 @@ const SearchMenu = () => {
   }
 
   return (
-    <div className={`h-full w-full bg-black md:w-[395px] `}>
-      <h1 className="font-semibold text-2xl pt-5 px-4 hidden md:block">Search</h1>
-      <div className="px-2 flex items-center gap-1 mt-2 md:px-4 md:mt-10">
-        <span className="md:hidden">
-          <ButtonBack />
-        </span>
-        <input
-          type="search"
-          autoFocus
-          className="bg-[#262626] w-full px-3 py-2 rounded-lg outline-none"
-          placeholder="Search"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div className="flex items-center mt-4 mb-4 justify-between px-2 md:px-5 md:mt-10 md:mb-6">
-        <p className="font-semibold md:text-[#A8A8A8]">Recent</p>
-        <button onClick={handleClearHistory} className="text-[#0694F1] hover:text-white">
-          Clear All
-        </button>
+    <div className={`h-full w-full bg-black md:w-[395px] overflow-y-auto`}>
+      <div className="sticky top-0 z-50 bg-black">
+        <h1 className="font-semibold text-2xl pt-5 px-4 hidden md:block">Search</h1>
+        <div className="px-2 flex items-center gap-1 mt-2 md:px-4 md:mt-10">
+          <span className="md:hidden">
+            <ButtonBack />
+          </span>
+          <input
+            type="search"
+            autoFocus
+            className="bg-[#262626] w-full px-3 py-2 rounded-lg outline-none"
+            placeholder="Search"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="flex items-center mt-4 mb-4 justify-between px-2 md:px-5 md:mt-10 md:mb-6">
+          <p className="font-semibold md:text-[#A8A8A8]">Recent</p>
+          <button onClick={handleClearHistory} className="text-[#0694F1] hover:text-white">
+            Clear All
+          </button>
+        </div>
       </div>
       {username.length === 0
         ? histories?.map((history) => (
